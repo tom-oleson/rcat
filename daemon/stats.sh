83bf71b617e8d7079598423db9ccd2fdd2bff36c
tom.oleson <tom.oleson@gmail.com>
2019-12-17 03:39:00 -0500

    Initial version

---
+++ daemon/stats.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+VORTEX=$1
+PORT=$2
+
+# use POSOX locale for correct number conversions in awk
+LC_NUMERIC=POSIX
+
+
+# use shared memory tmpfs (super fast)
+OUTFILE="/dev/shm/stats.out"
+RESFILE="/dev/shm/response.out"
+
+# clear any prevous stats output file
+rm -f ${OUTFILE}
+
+# write each line to our stats output file
+# each output adds +0 to fields to convert them to numbers sans decorators
+echo +$(hostname)_mem "{\"time\":"$(date +%s)","$(free | awk '/^Mem/{print "\"mem_used\":" $3+0 "," "\"mem_total\":" $2+0}')"}" >> ${OUTFILE}
+echo +$(hostname)_swap "{\"time\":"$(date +%s)","$(free | awk '/^Swap/{print "\"swap_used\":" $3+0 "," "\"swap_total\":" $2+0}')"}" >> ${OUTFILE}
+echo +$(hostname)_temp "{\"time\":"$(date +%s)","$(sensors | awk '/^temp1:/ { print "\"cpu_temp\":" $2+0.0}')"}" >> ${OUTFILE}
+echo +$(hostname)_sda1 "{\"time\":"$(date +%s)","$(df -H | awk '/sda1/ {print "\"sda1_used\":" $3+0 "," "\"sda1_avail\":" $4+0}')"}" >> ${OUTFILE}
+
+#output stats to vortex
+rcat -d50 ${VORTEX} ${PORT} ${OUTFILE} > ${RESFILE}
---
+++ daemon/vortex_stats.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+NAME=vortex_stats
+DESC="Stats to VORTEX"
+PIDFILE="/var/run/${NAME}.pid"
+LOGFILE="/var/log/${NAME}.log"
+
+# bin path
+DAEMON="/bin/sh"
+
+# Path the script
+DAEMON_OPTS="/var/opt/vortex/stats.sh"
+
+START_OPTS="--start --background --make-pidfile --pidfile ${PIDFILE} --exec ${DAEMON} ${DAEMON_OPTS}"
+STOP_OPTS="--stop --pidfile ${PIDFILE}"
+
+# does exec exit?
+test -x $DAEMON || exit 0
+
+set -e
+
+case "$1" in
+start)
+echo -n "Starting ${DESC}: "
+start-stop-daemon $START_OPTS >> $LOGFILE
+echo "$NAME."
+;;
+stop)
+echo -n "Stopping $DESC: "
+start-stop-daemon $STOP_OPTS
+echo "$NAME."
+rm -f $PIDFILE
+;;
+restart|force-reload)
+echo -n "Restarting $DESC: "
+start-stop-daemon $STOP_OPTS
+sleep 1
+start-stop-daemon $START_OPTS >> $LOGFILE
+echo "$NAME."
+;;
+*)
+N=/etc/init.d/$NAME
+echo "Usage: $N {start|stop|restart|force-reload}" >&2
+exit 1
+;;
+esac
+
+exit 0
