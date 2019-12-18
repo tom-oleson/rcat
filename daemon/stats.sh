1b31dfe8554c9040bc5eb5901a1c6dd5aea18374
rudogg420 <screamingroadglide@gmail.com>
2019-12-18 00:35:57 -0500

    add network stats

--- daemon/stats.sh
+++ daemon/stats.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VORTEX="tom-thinkpad-t4"
+VORTEX="192.168.1.232"
 PORT=54000
 
 # use POSOX locale for correct number conversions in awk
@@ -18,8 +18,9 @@ while true; do
     # each output adds +0 to fields to convert them to numbers sans decorators
     echo +$(hostname)_mem "{\"time\":"$(date +%s)","$(free | awk '/^Mem/{print "\"mem_used\":" $3+0 "," "\"mem_total\":" $2+0}')"}" >> ${OUTFILE}
     echo +$(hostname)_swap "{\"time\":"$(date +%s)","$(free | awk '/^Swap/{print "\"swap_used\":" $3+0 "," "\"swap_total\":" $2+0}')"}" >> ${OUTFILE}
-    echo +$(hostname)_temp "{\"time\":"$(date +%s)","$(sensors | awk '/^temp1:/ { print "\"cpu_temp\":" $2+0.0}')"}" >> ${OUTFILE}
-    echo +$(hostname)_sda1 "{\"time\":"$(date +%s)","$(df -H | awk '/sda1/ {print "\"sda1_used\":" $3+0 "," "\"sda1_avail\":" $4+0}')"}" >> ${OUTFILE}
+    #echo +$(hostname)_temp "{\"time\":"$(date +%s)","$(sensors | awk '/^temp1:/ { print "\"cpu_temp\":" $2+0.0}')"}" >> ${OUTFILE}
+    echo +$(hostname)_disk0 "{\"time\":"$(date +%s)","$(df -H | awk '/root/ {print "\"disk0_used\":" $3+0 "," "\"disk0_avail\":" $4+0}')"}" >> ${OUTFILE}
+    echo +$(hostname)_network "{\"time\":"$(date +%s)","$(cat /proc/net/dev | awk '/wlan0/ {print "\"net_recv\":" $2+0 "," "\"net_send\":" $9+0}')"}" >> ${OUTFILE}
 
     #output stats to vortex
     /opt/vortex/rcat -k1 -d50 ${VORTEX} ${PORT} ${OUTFILE} > ${RESFILE}
